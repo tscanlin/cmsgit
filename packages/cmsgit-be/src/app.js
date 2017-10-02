6c48f03673c6dc1298390dd1d10e2573463b909e
Tim Scanlin <tscanlin@gmail.com>
2017-10-01 17:02:54 -0700

    get lint working

--- packages/cmsgit-be/src/app.js
+++
@@ -1,7 +0,0 @@
-const http = require('http')
-
-exports.index = function(event, context, callback) {
-    http.createServer((req, res) => {
-        res.end('Hello World\n')
-    }).listen(process.env.PORT)
-}
--- packages/cmsgit-fe/pages/index.js
+++ packages/cmsgit-fe/pages/index.js
@@ -4,25 +4,26 @@ console.log(Head)
 
 const clientId = '4ea695b1a963ea3efab2'
 const scope = 'user:email,public_repo,repo'
+const redirectUri = 'http://localhost:3000/'
+const randomString = Math.random().toString(36)
 
-export default function Index(props) {
-    return (
-        <div>
-            <Head>
-                <title>Cmsgit</title>
-                <meta charSet="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="stylesheet" href="https://unpkg.com/tachyons@4.8.0/css/tachyons.min.css"/>
-            </Head>
-            <main className="sans-serif ma4">
-                <h1>
-                    Test!
-                </h1>
-                <a href={`https://github.com/login/oauth/authorize?scope=${scope}&client_id=${clientId}`}>
-                    Click here
-                </a>
-                
-            </main>
-        </div>
-    )
+export default function Index (props) {
+  return (
+    <div>
+      <Head>
+        <title>Cmsgit</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='stylesheet' href='https://unpkg.com/tachyons@4.8.0/css/tachyons.min.css' />
+      </Head>
+      <main className='sans-serif ma4'>
+        <h1>
+          Test!
+        </h1>
+        <a href={`https://github.com/login/oauth/authorize?scope=${scope}&client_id=${clientId}&redirect_uri=${redirectUri}&state=${randomString}`}>
+          Click here
+        </a>
+      </main>
+    </div>
+  )
 }
